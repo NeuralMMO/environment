9e275a734572cf2dbf423297ded76782a4b41e3f
jarbus <jarbus@tutanota.com>
2019-12-18 02:10:26 -0500

    better data transmit + backend for multiple-views

--- forge/blade/core/market/new_visualizer.py
+++ forge/blade/core/market/new_visualizer.py
@@ -10,39 +10,52 @@ from random import random
 from bokeh.server.server import Server
 from bokeh.models import ColumnDataSource
 from bokeh.models import Band
+from bokeh.models.widgets import RadioButtonGroup
+from bokeh.layouts import layout
+
 from bokeh.plotting import curdoc, figure, show
 from bokeh.themes import Theme
 
 from tornado import gen
 from tornado.ioloop import IOLoop
 
-from forge.blade.core.market.config import *
-
-# to run a demo with dummy data:
-# $ bokeh serve --show visualizer.py
+from config import *
 
 # market plans
 # click on objects in market to display stats about them
 # market overview tab
 # -> trade bandwidth, demand, gdp
 
-PORT=5009
-
 class MarketVisualizer:
-    def __init__(self, keys, history_len: int = 512,
-                 title: str = "NeuralMMO Market Data", x: str = "tick",
+    def __init__(self,
+                 keys,
+                 scales: list = [1],
+                 history_len: int = 256,
+                 title: str = "NeuralMMO Market Data",
+                 x: str = "tick",
                  ylabel: str = "Dummy Values"):
         """Visualizes a stream of data with threaded refreshing
-    
+
         Args:
             keys        : List of object names (str) to be displayed on market
             history_len : How far back to plot data
+            scales      : Scales for visualization
             title       : Title of graph
             x           : Name of x axis data
             ylabel      : Name of y axis on plot
         """
         self.colors = 'blue red green yellow orange purple'.split()
         self.data        = {}
+
+        # Market stores data in a dictionary of lists
+        # MarketVisualizer stores the data for each view as a dictionary,
+        # inside another dictionary with keys of each scale.
+
+        self.scales = scales
+        for scale in self.scales:
+            self.data[scale] = {}
+        self.scale = self.scales[0]
+
 
         self.history_len = history_len
         self.title       = title
@@ -54,31 +67,37 @@ class MarketVisualizer:
         # TODO figure out a better way to ensure unique colors
         assert len(self.keys) <= len(self.colors), 'Limited color pool'
 
-        for i, key in enumerate(self.keys):
-            self.data[key] = []
-            self.data[key+'lower'] = []
-            self.data[key+'upper'] = []
-        self.data['tick'] = []
+        # data initialization
+        for scale in self.scales:
+            for i, key in enumerate(self.keys):
+                self.data[scale][key] = []
+                self.data[scale][key+'lower'] = []
+                self.data[scale][key+'upper'] = []
+            self.data[scale][self.x] = []
+            # Isn't necessary, but makes code for packet handling nicer
+            self.data[scale][self.x + 'upper'] = []
+            self.data[scale][self.x + 'lower'] = []
 
 
     def init(self, doc):
-        #Source must only be modified through a stream
-        self.source = ColumnDataSource(data=self.data)
+        # Source must only be modified through a stream
+        self.source = ColumnDataSource(data=self.data[self.scale])
 
-        #Enable theming
-        theme = Theme('forge/blade/core/market/theme.yaml')
+        # Enable theming
+        theme = Theme('theme.yaml')
         doc.theme = theme
         self.doc = doc
+
 
         fig = figure(
            plot_width=600,
            plot_height=400,
-           tools='pan,xwheel_zoom,box_zoom,save,reset',
+           tools='xpan,xwheel_zoom, xbox_zoom, reset',
            title='Neural MMO: Market Data',
            x_axis_label=self.x,
            y_axis_label=self.ylabel)
 
-        #Initialize plots
+        # Initialize plots
         for i, key in enumerate(self.keys):
             fig.line(
                 source=self.source,
@@ -101,14 +120,22 @@ class MarketVisualizer:
                fill_alpha=0.2)
             fig.add_layout(band) 
 
-        #Set root
-        self.doc.add_root(fig)
+        def switch_scale(attr, old, new):
+            self.scale = self.scales[new]
+
+        timescales = RadioButtonGroup(
+            labels=[str(scale) for scale in self.scales], active=0)
+        timescales.on_change('active', switch_scale)
+
+        structure = layout([[timescales], [fig]])
+        self.doc.add_root(structure)
+
         self.fig = fig
 
 @ray.remote
 class BokehServer:
     def __init__(self, market, *args, **kwargs):
-        """ Runs an asynchronous Bokeh data streaming server
+        """ Runs an asynchronous Bokeh data streaming server.
       
         Args:
            market : The market to visualize
@@ -127,6 +154,13 @@ class BokehServer:
         self.server = server
         self.thread = None
         self.tick   = 0
+        self.packet = {}
+
+        if 'scales' in kwargs and type(kwargs['scales']) is list:
+            self.scales = kwargs['scales']
+        else:
+            self.scales = [1]
+
         server.start()
 
         server.io_loop.add_callback(server.show, "/")
@@ -134,7 +168,7 @@ class BokehServer:
 
     def init(self, doc):
         '''Initialize document and threaded update loop
-      
+
         Args:
            doc: A Bokeh document
         '''
@@ -147,43 +181,54 @@ class BokehServer:
 
     def update(self):
         '''Blocking update call to be run in a separate thread
-        
+
         Ingests packets from a remote market and streams to Bokeh client'''
         self.n = 0
         while True:
-            #Wait for thread to initialize
+            # Wait for thread to initialize
             time.sleep(0.05)
             if self.thread is None:
                continue 
 
-            #Get remote market data
+            # Get remote market data
             packet = ray.get(self.market.getData.remote())
             if packet is None:
                continue
+            self.packet = packet.copy() if type(packet) == dict else {}
 
-            #Ingest market data
+            # Ingest market data, add upper and lower values for each scale
+            # self.visu.data stores all data
+            # self.packet adds lower and upper to current packet to stream
+
             for key, val in packet.items():
-              if key[-3:] == 'std':
-                 key = key[:-4]
-                 dat = packet[key]
-                 self.visu.data[key + 'lower'].append(dat - val)
-                 self.visu.data[key + 'upper'].append(dat + val)
-              else:
-                 self.visu.data[key].append(val)
+                for scale in self.scales:
+                    # Ignore any irrelevant buffers
+                    if packet['tick'] % scale != 0:
+                        continue
+                    self.visu.data[scale][key].append(val)
+                    self.visu.data[scale][key + 'lower'].append(val - 0.1)
+                    self.visu.data[scale][key + 'upper'].append(val + 0.1)
+                self.packet[key] = [self.packet[key]]
+                self.packet[key + 'lower'] = [val - 0.1]
+                self.packet[key + 'upper'] = [val + 0.1]
 
-            #Stream to Bokeh client
+            # Stream to Bokeh client
             self.doc.add_next_tick_callback(partial(self.stream))
             self.tick += 1
 
     @gen.coroutine
     def stream(self):
         '''Stream current data buffer to Bokeh client'''
-        self.visu.source.stream(self.visu.data, self.visu.history_len)
+        # TODO update with better buffers
+        # visu is visualizer object
+        self.visu.source.stream(self.packet, self.visu.history_len)
+
 
 @ray.remote
 class Middleman:
     def __init__(self):
-        '''Remote data buffer for two processes to dump and recv data
+        '''Remote data buffer for two processes to dump and recv data.
+        Interacts with Market and BokehServer.
 
         This is probably not safe'''
         self.data = None
@@ -204,7 +249,7 @@ class Middleman:
         Args:
            data: To set buffer
         '''
-        self.data = data
+        self.data = data.copy()
 
 class Market:
     def __init__(self, items, middleman):
@@ -226,7 +271,7 @@ class Market:
 
     def update(self):
         '''Updates market data and propagates to Bokeh server
-
+        # update data and send to middle man
         Note: best to update all at once. Current version may cause bugs'''
         for key, val in self.data.items():
             self.data[key] = val + 0.2*(random() - 0.5)
@@ -237,15 +282,15 @@ class Market:
         self.middleman.setData.remote(self.data)
 
 # Example setup
-if __name__ == '__main__':
-   ray.init()
-   ITEMS = ['Food', 'Water', 'Health', 'Melee', 'Range', 'Mage']
+PORT=5009
+ray.init()
+ITEMS = ['Food', 'Water', 'Health', 'Melee', 'Range', 'Mage']
+SCALES = [1, 10, 100, 1000]
 
-   middleman  = Middleman.remote()
-   market     = Market(ITEMS, middleman)
-   visualizer = BokehServer.remote(middleman, ITEMS)
+middleman  = Middleman.remote()
+market     = Market(ITEMS, middleman)
+visualizer = BokehServer.remote(middleman, ITEMS, scales=SCALES)
 
-   while True:
-     time.sleep(0.1)
-     market.update()
-
+while True:
+  time.sleep(0.1)
+  market.update()
