8ae6116ffa80634f6e340cb52ad7bd25586dfe2b
David Bloomin <daveey@gmail.com>
2023-06-12 17:00:55 -0700

    make market_n_obs a constant

--- tests/test_memory_usage.py
+++ tests/test_memory_usage.py
@@ -1,11 +1,12 @@
+# pylint: disable=bad-builtin, unused-variable
 import psutil
 
 import nmmo
 
 def test_memory_usage():
-    env = nmmo.Env()
-    process = psutil.Process()
-    print(process.memory_info().rss)
+  env = nmmo.Env()
+  process = psutil.Process()
+  print("memory", process.memory_info().rss)
 
 if __name__ == '__main__':
-    test_memory_usage()
+  test_memory_usage()
