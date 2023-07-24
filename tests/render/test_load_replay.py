ae666aaaf86e2a5cf0769132668656fd0c234beb
kywch <choe.kyoung@gmail.com>
2023-07-24 10:12:08 +0900

    removed decompress flag

--- tests/render/test_load_replay.py
+++ tests/render/test_load_replay.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
   time.sleep(3)
 
   # load a replay: replace 'replay_dev.json' with your replay file
-  replay = FileReplayHelper.load('replay_dev.json', decompress=False)
+  replay = FileReplayHelper.load('replay_dev.json')
 
   # run the replay
   for packet in replay:
